5b78b45d1f7f4f51e74c490c3bfbb795236b35da
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-08 00:37:06 +0300

    Longest Word problemi çözüldü

---
+++ LongestWord/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LongestWord
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestWord(Console.ReadLine()));
+        }
+        public static string LongestWord(string sen)
+        {
+
+            Regex rgx = new Regex(@"[^\w\s]");
+            sen = rgx.Replace(sen, "");
+            string[] words = sen.Split(' ');
+
+            return words.OrderByDescending(x => x.Length).First();
+            //return Regex.Replace(sen, @"(\p{P})", "").Split(' ').OrderByDescending(i => i.Length).First();
+        }
+    }
+
+   
+}
+
