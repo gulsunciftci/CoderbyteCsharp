fa3d57eec004a51a7d88011a0b58d423776ef941
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-07 23:02:42 +0300

    First Reverse problemi çözüldü

---
+++ FirstReverse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstReverse(Console.ReadLine()));
+        }
+        public static string FirstReverse(string str)
+        {
+
+
+            string sol =String.Empty;
+            foreach (char c in str)
+            {
+                sol = sol+c;
+                Console.WriteLine(sol);
+
+            }
+
+
+
+            return sol;
+
+        }
+
+    }
+}
