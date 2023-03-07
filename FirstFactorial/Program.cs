394502e3c6a9dd68e0919f83352193b39ea3bbcc
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-07 23:21:51 +0300

    First Factorial problemi çözüldü

---
+++ FirstFactorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstFactorial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstFactorial(Convert.ToInt32(Console.ReadLine())));
+        }
+        public static int FirstFactorial(int num)
+        {
+
+            var fact = 1;
+            for (int i = num; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            // code goes here  
+            return fact;
+
+        }
+    }
+}
