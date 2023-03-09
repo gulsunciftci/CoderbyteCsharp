2068e1d9331ece5757fae899c726534a7b810aa5
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-09 04:29:29 +0300

    Bracket Matcher problemi çözüldü

---
+++ Bracket Matcher/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BracketMatcher
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BracketMatcher(Console.ReadLine()));
+        }
+        public static string BracketMatcher(string str)
+        {
+
+            // code goes here  
+            str = Regex.Replace(str, "[^()]", String.Empty);
+            char[] a = str.ToCharArray();
+            int numOpens = 0;
+            foreach (char p in a)
+            {
+                if (p == '(')
+                {
+                    numOpens++;
+                }
+                if (p == ')')
+                {
+                    if (numOpens <= 0)
+                    {
+                        return "0";
+                    }
+                    else
+                    {
+                        numOpens--;
+                    }
+                }
+            }
+
+            if (numOpens > 0)
+            {
+                return "0";
+            }
+
+            return "1";
+        }
+    }
+}
---
+++ FindIntersection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindIntersection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string strings = " ";
+            strings=Console.ReadLine();
+            String[] strings1 = strings;
+            Console.WriteLine(FindIntersection(strings.ToArray());
+        }
+        public static string FindIntersection(string[] strArr)
+        {
+            string resultString = string.Join(",", strArr[0].Split(',').Select(x=>x.Trim()).Intersect(strArr[1].Split(',').Select(x => x.Trim())));
+            return string.IsNullOrEmpty(resultString) ? "false" : resultString;
+
+        }
+    }
+}
