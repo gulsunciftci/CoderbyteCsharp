d8de8940fcd0f79134574f47ab4a89aae6ceb86a
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-21 03:26:23 +0300

    CoderByte Letter Changes problemi çözüldü

---
+++ LetterChanges/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterChanges
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LetterChanges(Console.ReadLine()));
+        }
+        public static string LetterChanges(string str)
+        {
+            char[] strArr = str.ToCharArray();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (char.IsLetter(strArr[i]) )
+                {
+                    
+                    strArr[i]++;
+
+                    if (!char.IsLetter(strArr[i]))
+                    {
+                        strArr[i] = 'A';
+                    }
+
+                    switch (Char.ToLower(strArr[i]))
+                    {
+                        case 'a':
+                        case 'e':
+                        case 'i':
+                        case 'o':
+                        case 'u':
+                            strArr[i] = Char.ToUpper(strArr[i]);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                sb.Append(strArr[i]);
+            }
+
+            str = sb.ToString();
+            return str;
+
+        }
+    }
+}
