73d4c252d7551828acab0a27efbd6432cf63024b
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-21 14:29:56 +0300

    CoderByte  AB Check problemi çözüldü

---
+++ ABCheck/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCheck
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = Console.ReadLine();
+            Console.WriteLine(Result(str));
+
+        }
+
+        public static string Result(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.ToLower(str[i]) == 'a' && (i + 4) < str.Length && char.ToLower(str[i + 4]) == 'b')
+                {
+                    return "true";
+
+                }
+                else if (char.ToLower(str[i]) == 'b' && (i + 4) < str.Length && char.ToLower(str[i + 4]) == 'a')
+                {
+                    return "true";
+
+                }
+            }
+            return "false";
+
+        }
+    }
+}
