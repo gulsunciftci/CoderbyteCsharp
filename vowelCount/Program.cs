69e2d929937a3d6bc9ac072e504a00b990442e13
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-21 14:48:35 +0300

    CoderByte Vowel Count problemi çözüldü

---
+++ vowelCount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vowelCount
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int count = 0;
+            foreach (char c in str)
+            {
+                switch (c)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        count++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
