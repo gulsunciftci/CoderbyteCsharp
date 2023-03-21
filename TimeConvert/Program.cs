571f8c5837c70ae1c7295f0cfc401e294068474f
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-21 04:59:13 +0300

    CoderByte Time Convert problemi çözüldü

---
+++ TimeConvert/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConvert
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int Time = int.Parse(Console.ReadLine());
+            int hour = Time / 60;
+            int minute = Time % 60;
+            Console.WriteLine($"{ hour}:{minute }");
+        }
+    }
+}
