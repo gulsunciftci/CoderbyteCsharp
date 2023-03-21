9c801946cf07d501544bb8147aeac4aa36239ffd
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-21 05:05:49 +0300

    CoderByte  Alphabet Soup problemi çözüldü

---
+++ AlphabetSoup/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetSoup
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            str = String.Concat(str.OrderBy(s=>s));
+            Console.WriteLine(str);
+        }
+    }
+}
