20b6a962214f983e122166d33e713b0c21dae042
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-21 16:10:11 +0300

    CoderByte Ex Oh problemi çözüldü

---
+++ exOh/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exOh
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(Result(str));
+        }
+
+        public static string Result(string str)
+        {
+            int countX = 0;
+            int countO = 0;
+            foreach (char s in str)
+            {
+                if (char.ToLower(s) == 'x')
+                {
+                    countX++;
+                }
+                else if (char.ToLower(s) == 'o')
+                {
+                    countO++;
+                }
+                else
+                {
+                    return "false";
+                }
+            }
+            if (countX == countO)
+            
+                return "true";
+            
+            else  
+                return "false";
+           
+        }
+    }
+}
