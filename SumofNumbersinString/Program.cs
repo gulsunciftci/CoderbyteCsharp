94ab688f96d0b3f6563a762adb37aad0fa0738b6
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-29 00:19:04 +0300

    Bir string içindeki sayıların toplamını bulan program yazıldı

---
+++ SumofNumbersinString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumofNumbersinString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(findSum(Console.ReadLine()));
+        }
+        public static int findSum(string str)
+        {
+            String temp = "0";
+            int sum = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+
+                if (char.IsDigit(ch))
+                    temp += ch;
+                else
+                {
+                    sum += int.Parse(temp);
+                    temp = "0";
+                }
+            }
+            return sum + int.Parse(temp);
+        }
+    }
+}
